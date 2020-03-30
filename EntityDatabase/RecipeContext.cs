7f916b0d166d3648ab477d669d9dbebf11bfa6ab
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-03-30 17:56:30 -0400

    fixed typo and name of database

--- EntityDatabase/RecipeContext.cs
+++ EntityDatabase/RecipeContext.cs
@@ -6,10 +6,11 @@ namespace RecipeWebApplication.EntityDatabase
 {
     public class RecipeContext: DbContext
     {
-        public DbSet<Ingridient> Ingridients { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Type> Types { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder
-                .UseMySQL("server=localhost;database=library;user=sveta;password=123");       
+                .UseMySQL("server=localhost;database=recipeDb;user=sveta;password=123");       
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RecipeWebApplication.Repositories;
 
 namespace RecipeWebApplication
 {
@@ -22,5 +23,6 @@ namespace RecipeWebApplication
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
