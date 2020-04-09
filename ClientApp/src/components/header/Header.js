4c34b53398412ecdc9093e10e133333f0b6181d5
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-09 16:13:45 -0400

    created logo and memu

--- ClientApp/src/components/header/Header.js
+++ ClientApp/src/components/header/Header.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import logo from './logo.png';
+import './Header.css';
 
 class Header extends React.Component {
+    filter() {
+        alert("It works!");
+    }
+
     render() {
         return (
             <div>
-                <h2>This is Header</h2>
+                <header className="header">
+                        <div className="logo column">
+                            <img src={logo} alt="logo" />
+                            Homemade Food
+                        </div>
+                        <nav role="filtration" className="nav-menu column">
+                            <ul id="menu" className="filter-menu">
+                                <li><button onClick={this.filter}>Breakfast</button></li>
+                                <li><button onClick={this.filter}>Lunch</button></li>
+                                <li><button onClick={this.filter}>Dinner</button></li>
+                            </ul>
+                        </nav>
+                </header>    
             </div>
         );
     }
