38655cb7f0a08a0d5d4f7bf3fdafaf594e3a9a1d
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-13 16:44:56 -0400

    created Gallery component

---
+++ ClientApp/src/components/gallery/Gallery.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Gallery.css';
+import  Recipe from '../recipe/Recipe';
+
+class Gallery extends React.Component {
+    render() {
+        return (
+            <div className="gallery-wrapper">
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+                <Recipe/>
+            </div>
+        );
+    }
+}
+
+export default Gallery;
