e8f2f42b0a6d6c3933e3fefeccdfc157dd1f61ff
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-13 16:47:35 -0400

    created Create component

--- ClientApp/src/components/createRecipe/Create.js
+++ ClientApp/src/components/createRecipe/Create.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './Create.css';
+import ChickenDrumstick from './ChickenDrumstick.jpg';
 
 class Create extends React.Component {
+    save(){
+        alert("New recipe saved");
+    }
     render() {
         return (
-            <div>
-                <h1>Create new recipe</h1>
+            <div className="create-container">
+                <h2>Create new recipe</h2>
+                <img src={ChickenDrumstick} className="img-item" />
+                <form action="/action_page.php" className="upload-form">
+                    <label for="img">Select image:</label>
+                    <input type="file" id="img" name="img" accept="image/*"></input>
+                    <input type="submit"></input>
+                </form>
+                <form className="recipe-form-items">
+                    <label for="recipe-name">Name:</label>
+                    <input type="text" id="recipe-name" name="recipe-name"></input><br />
+                    <label for="type">Type:</label>
+                    <input type="text" id="type" name="type"></input><br />
+                    <label for="ingredients">Ingredients:</label>
+                    <input type="text" id="ingredients" name="ingredients"></input><br />
+                    <label for="description">Description:</label>
+                    <input type="text" id="description" name="description"></input><br />
+                </form>
+                <button onClick={this.save} className="save-button">Save</button>
             </div>
         );
     }
