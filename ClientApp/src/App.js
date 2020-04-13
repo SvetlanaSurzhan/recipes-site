c20de2971fab48415ff01cfacc95f9cbb13356f1
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-13 16:48:20 -0400

    added Gallery component

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { ReactDOM } from 'react-dom';
 import  Create from './components/createRecipe/Create';
-import  Recipe  from './components/gallery/Recipe';
+import  Recipe  from './components/recipe/Recipe';
 import  Update  from './components/updateRecipe/Update';
 import  Shell  from './components/shell/Shell';
 import './custom.css'
+import Gallery from './components/gallery/Gallery';
 
 
 export default class App extends Component {
@@ -16,7 +17,7 @@ export default class App extends Component {
       <div>
         <Shell>
             <Create />
-            <Recipe />
+            <Gallery />
             <Update />
         </Shell>
       </div>
