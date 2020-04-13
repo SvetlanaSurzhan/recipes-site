ff3c97786ad6f17b655d29adc7f464fcc766f79f
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-13 17:35:44 -0400

    added Router

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { ReactDOM } from 'react-dom';
-import  Create from './components/createRecipe/Create';
-import  Recipe  from './components/recipe/Recipe';
-import  Update  from './components/updateRecipe/Update';
-import  Shell  from './components/shell/Shell';
+import Create from './components/createRecipe/Create';
+import Update from './components/updateRecipe/Update';
+import Shell from './components/shell/Shell';
 import './custom.css'
 import Gallery from './components/gallery/Gallery';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
-      <div>
-        <Shell>
-            <Create />
-            <Gallery />
-            <Update />
-        </Shell>
-      </div>
+      <Shell>
+        <Router>
+          <div>
+            <Route path='/create' component={Create} />
+            <Route path='/update/:id' component={Update} />
+            <Route exact path='/' component={Gallery} />
+          </div>
+        </Router>
+      </Shell>
     );
   }
 }
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -9,9 +8,7 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+    <App />,
   rootElement);
 
 registerServiceWorker();
