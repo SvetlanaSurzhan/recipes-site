67947f74801d43c1fefe1e6a39b93cf1b86b999c
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-12 22:26:52 -0400

    created contact and social links

--- ClientApp/src/components/footer/Footer.js
+++ ClientApp/src/components/footer/Footer.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import instagram from './instagram.png';
+import facebook from './facebook.png';
+import linkedin from './linkedin.png';
 
 class Footer extends React.Component {
     render() {
         return (
             <div>
-                <footer className="nav-footer">
-                    <h3>Footer</h3>
+                <footer className="footer-container">
+                    <div className="contact-item">
+                        <h4>Contact</h4>
+                        <address>
+                            Louisville, KY 40245<br />
+                            E: svetlana.d.surzhan@gmail.com<br />
+                            P: 502-291-6157
+                        </address>
+                    </div>
+                    <div className="social-links-item">
+                        <h4>Follow Us</h4>
+                        <ul>
+                            <li><image src={instagram} className="instagram-icon" /></li>
+                            <li><image src={facebook} className="facebook-icon" /></li>
+                            <li><image src={linkedin} className="linkedin-icon" /></li>
+                        </ul>
+                    </div>
+
                 </footer>
             </div>
         );
