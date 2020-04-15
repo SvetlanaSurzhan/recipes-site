6740ef00296cf2bd2bacd5fe25aa0c6d6487b7a0
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-15 10:12:48 -0400

    added  props recipeCake

--- ClientApp/src/components/recipe/Recipe.js
+++ ClientApp/src/components/recipe/Recipe.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import './Recipe.css';
 import ChickenDrumstick from './ChickenDrumstick.jpg';
+import recipeCake from './recipeCake'
 
 class Recipe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipeCake: recipeCake
+        };
+    }
     render() {
         return (
             <div className="recipe-container">
-                <div className="recipe-items">
-                    <img src={ChickenDrumstick} className="img-item" />
-                    <h5>Recipe1</h5>
-                    <p>Ingredients: <br />
+                {recipeCake.map(cake => {
+                    return (
+                        <div className="recipe-items">
+                            <h5>{cake.name}</h5>
+                            <h6>Ingredients:</h6>
+                            <ul>
+                                {
+                                    cake.ingredients.map((ingr) => {
+                                        return (
+                                            <li key={ingr.id} align="start">
+                                                {ingr.name}
+                                                {ingr.quantity}
+                                                {ingr.units}
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+                            <p>{cake.description}</p>
+                        </div>
+                    );
+                })}
+
+                {/* <img src={ChickenDrumstick} className="img-item" />
+                            <h5>Recipe1</h5>
+                            <p>Ingredients: <br />
                     1. Chicken drumstick <br />
                     2. Sugar <br />
-                    </p>
-                </div>
+                            </p> 
+                            <img className="img-item">{cake.picture}</img>*/}
             </div>
         );
     }
