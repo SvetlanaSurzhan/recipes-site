6b7fd020cede33504111bb54061281106d8e9ed3
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-16 17:07:34 -0400

    deleted nav buttons and style for them

--- ClientApp/src/components/header/Header.js
+++ ClientApp/src/components/header/Header.js
@@ -3,26 +3,15 @@ import logo from './logo.png';
 import './Header.css';
 
 class Header extends React.Component {
-    filter() {
-        alert("It works!");
-    }
-
     render() {
         return (
             <div>
                 <header className="header-container">
-                        <div className="logo-item">
-                            <img src={logo} alt="logo" className="image-item"/>
-                            <h3>Homemade Food</h3>
-                        </div>
-                        <nav role="filtration" className="menu-item">
-                            <ul id="menu" className="filter-menu">
-                                <li><button onClick={this.filter}>Breakfast</button></li>
-                                <li><button onClick={this.filter}>Lunch</button></li>
-                                <li><button onClick={this.filter}>Dinner</button></li>
-                            </ul>
-                        </nav>
-                </header>    
+                    <div className="logo-item">
+                        <img src={logo} alt="logo" className="image-item" />
+                        <h3>Homemade Food</h3>
+                    </div>
+                </header>
             </div>
         );
     }
