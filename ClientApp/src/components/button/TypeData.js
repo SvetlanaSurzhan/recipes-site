2d6a82d3520d8795febea2c7da8fede523dfa208
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-18 22:28:01 -0400

    created file of recipe types for testing

---
+++ ClientApp/src/components/button/TypeData.js
@@ -0,0 +1,19 @@
+const type = [
+    {
+        "name": "Breakfast",
+        "typeId": 1
+    },
+    {
+        "name": "Lunch",
+        "typeId": 2
+    },
+    {
+        "name": "Dinner",
+        "typeId": 3
+    },
+    {
+        "name": "Test",
+        "typeId": 4
+    }
+]
+export default type;    
