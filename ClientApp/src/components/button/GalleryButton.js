ec3df87c4380d376c8ba4c0b791e02cd3b726efe
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-18 22:37:28 -0400

    refactor the button component to accept type of recipe

--- ClientApp/src/components/button/GalleryButton.js
+++ ClientApp/src/components/button/GalleryButton.js
@@ -2,17 +2,12 @@ import React, { Component } from 'react';
 import './GalleryButton.css';
 
 class GalleryButton extends React.Component {
-    filter() {
-        alert("It works!");
-    }
     render() {
         return (
-            <div className="button-wrapper">
+            <div>
                 <nav role="filtration" className="menu-item">
                     <ul className="filter-menu">
-                        <li><button onClick={this.filter}>Breakfast</button></li>
-                        <li><button onClick={this.filter}>Lunch</button></li>
-                        <li><button onClick={this.filter}>Dinner</button></li>
+                        <button>{this.props.typeOfRecipe.name}</button>
                     </ul>
                 </nav>
             </div>
--- ClientApp/src/components/gallery/Gallery.js
+++ ClientApp/src/components/gallery/Gallery.js
@@ -3,19 +3,25 @@ import './Gallery.css';
 import Recipe from '../recipe/Recipe';
 import posts from '../recipe/recipeData';
 import GalleryButton from '../button/GalleryButton';
+import recipeType from '../button/TypeData';
 
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: posts
+            posts: posts,
+            recipeType: recipeType
         };
     }
     render() {
         return (
             <div className="gallery-wrapper">
                 <div className="button-items">
-                    <GalleryButton/>
+                {this.state.recipeType.map((type) => {
+                        return (
+                            <GalleryButton  typeOfRecipe={type}/>
+                        );
+                    })}
                 </div>
                 <div className="recipe-item">
                     {this.state.posts.map((cake) => {
