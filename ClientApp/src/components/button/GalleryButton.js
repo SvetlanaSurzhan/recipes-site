9843cb133e69182e9a1ebfb860e8f961de04c249
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-20 12:57:53 -0400

    added button All to see all recipes

--- ClientApp/src/components/button/GalleryButton.js
+++ ClientApp/src/components/button/GalleryButton.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './GalleryButton.css';
+import Gallery from '../gallery/Gallery';
 
 class GalleryButton extends React.Component {
     render() {
+        const recipeType = this.props.recipeType;
+        const buttonName = recipeType ? recipeType.name : 'All';
+        const typeId = recipeType ? recipeType.typeId : undefined;
+
         return (
             <div>
-                <nav role="filtration" className="menu-item">
-                    <ul className="filter-menu">
-                        <button>{this.props.typeOfRecipe.name}</button>
-                    </ul>
+                <nav role="filtration" className="filter-menu">
+                    <button onClick={(e) => this.props.onCLickHandler(typeId)}>
+                        {buttonName}
+                    </button>
                 </nav>
             </div>
         );
