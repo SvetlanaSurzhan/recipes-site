e274e20051b28747f79d2102bd2b56b74069dbf2
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-22 16:03:47 -0400

    created input form for Create and Update pages

--- ClientApp/src/components/inputForm/InputForm.js
+++ ClientApp/src/components/inputForm/InputForm.js
@@ -1,33 +1,189 @@
-import React, { Component } from 'react';
-import './InputForm.css';
+import React from "react";
+import "./InputForm.css";
 
 class InputForm extends React.Component {
-    save(){
-        alert("New recipe saved");
+    constructor(props) {
+        super(props);
+
+        let recipe =  {
+            recipeId: 0,
+            ingredients: [],
+            name: "",
+            description: "",
+            type: this.props.types[2],
+            picture: "",
+        };
+
+        if (this.props.recipe) {
+            recipe = {...this.props.recipe};
+        }
+
+        this.newIngredient = {
+            ingredientId: 0,
+            name: "",
+            quantity: 0,
+            units: "",
+        };
+
+        this.state = {
+            recipe,
+            newIngredient: {...this.newIngredient}
+        };
     }
-    render() {
+
+    handleRecipeChange = (key) => {
+        return (event) => {
+            const recipe = {...this.state.recipe}; 
+
+            recipe[key] = event.target.value;
+
+            this.setState({ recipe });
+
+            console.log(this.state.recipe);
+        };
+    };
+
+    handleDropDownSelect = (event) => {
+        const typeId = event.target.value;
+        const typeObj = this.props.types.find(t => t.typeId.toString() === typeId);
+        const recipe = {...this.state.recipe}; 
+
+        recipe.type = typeObj;
+
+        this.setState({ recipe });
+    }
+
+    handleIngredientChange = (key) => {
+        return (event) => {
+            const newIngredient = {...this.state.newIngredient}
+            const value = event.target.value;
+
+            //check if number
+            if (value && !isNaN(value)) {
+                newIngredient[key] = parseFloat(value);
+            } else {
+                newIngredient[key] = value;
+            }
+
+
+            this.setState({newIngredient});
+
+            console.log(this.state.newIngredient);
+        };
+    };
+
+    handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+        const that = this;
+        const recipe = {...this.state.recipe}
+
+        fileReader.addEventListener("load", function () {
+            recipe.picture = fileReader.result
+
+            that.setState({
+                recipe
+            });
+
+            console.log('Read file and store into state', that.state.recipe)
+          }, false);
+
+        fileReader.readAsDataURL(file);
+    };
+
+    handleAddIngredient = (event) => {
+        event.preventDefault();
+        const recipe = {...this.state.recipe};
+        recipe.ingredients.push(this.state.newIngredient);
+
+        this.setState({ recipe });
+        this.setState({ newIngredient: {...this.newIngredient} }); 
+    }
+
+    handleSubmit = (recipe) => {
+        return (event) => {
+            event.preventDefault();
+            this.props.onSave(recipe)
+        }
+    }
+
+    
+    render() {  
         return (
             <div className="form">
-                <img className="img-item" />
-                <form action="/action_page.php" className="upload-form">
-                    <label for="img">Select image:</label>
-                    <input type="file" id="img" name="img" accept="image/*"></input>
-                    <input type="submit"></input>
+                <h4>Recipe:</h4>
+                <form onSubmit={this.handleSubmit(this.state.recipe)} className="recipe-form">
+                    <img className="img-item" src={this.state.recipe.picture} />
+
+                    <label htmlFor="img">Select image:</label>
+                    <input
+                        type="file"
+                        id="myImage"
+                        name="myImage"
+                        onChange={this.handleImageUpload}
+                        accept="image/*"
+                    ></input>
+
+                    <label htmlFor="recipe-name">Name:</label>
+                    <input
+                        type="text"
+                        onChange={this.handleRecipeChange("name")}
+                        value={this.state.recipe.name}
+                        id="recipe-name"
+                        name="recipe-name"
+                    ></input>
+                    <br />
+
+                    <label htmlFor="description">Description:</label>
+                    <input
+                        type="text"
+                        onChange={this.handleRecipeChange("description")}
+                        value={this.state.recipe.description}
+                        id="description"
+                        name="description"
+                    ></input>
+                    <br />
+
+                    <label>Select recipe type:</label>
+                    <select value={this.state.recipe.type.typeId} onChange={this.handleDropDownSelect}>
+                        {this.props.types.map((type, i) => (
+                            <option key={i} value={type.typeId}>{type.name}</option>
+                        ))}
+                    </select>
+
+                    <h4>Ingredients:</h4>
+                    <label htmlFor="ingredient-name">Name</label>
+                    <input
+                        type="text"
+                        onChange={this.handleIngredientChange("name")}
+                        value={this.state.newIngredient.name}
+                        id="ingredient-name"
+                        name="ingredient-name"
+                    ></input>
+                    <label htmlFor="ingredient-quantity">Quantity</label>
+                    <input
+                        type="number"
+                        onChange={this.handleIngredientChange("quantity")}
+                        value={this.state.newIngredient.quantity}
+                        id="ingredient-quantity"
+                        name="ingredient-quantity"
+                    ></input>
+                    <label htmlFor="ingredient-units">Units</label>
+                    <input
+                        type="text"
+                        onChange={this.handleIngredientChange("units")}
+                        value={this.state.newIngredient.units}
+                        id="ingredient-units"
+                        name="ingredient-units"
+                    ></input>
+                    <button onClick={this.handleAddIngredient}>
+                        Add Ingredient
+                        </button>
+                    <button type="submit">Save</button>
                 </form>
-                <form className="recipe-form-items">
-                    <label for="recipe-name">Name:</label>
-                    <input type="text" id="recipe-name" name="recipe-name"></input><br />
-                    <label for="type">Type:</label>
-                    <input type="text" id="type" name="type"></input><br />
-                    <label for="ingredients">Ingredients:</label>
-                    <input type="text" id="ingredients" name="ingredients"></input><br />
-                    <label for="description">Description:</label>
-                    <input type="text" id="description" name="description"></input><br />
-                </form>
-                <button onClick={this.save} className="save-button">Save</button>
             </div>
         );
-    } 
+    }
 }
 
 export default InputForm;
