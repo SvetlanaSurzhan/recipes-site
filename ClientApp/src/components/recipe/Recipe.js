5c0a62e4780ea5d8a7a64d7ec16a5dca373d272c
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-22 16:13:58 -0400

    added onClick function to recipe container and added key to ingredients

--- ClientApp/src/components/recipe/Recipe.js
+++ ClientApp/src/components/recipe/Recipe.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Recipe.css';
 
 class Recipe extends React.Component {
 
     render() {
         return (
-            <div className="recipe-container">
+            <div className="recipe-container" onClick={() => (this.props.history.push(`/update/${this.props.recipe.recipeId}`))}>
                 <div className="recipe-items">
-                    <img className="img-item" src={`data:image/png;base64,${this.props.recipe.picture}`} />
+                    <img className="img-item" alt={this.props.recipe.name} src={this.props.recipe.picture} />
                     <h5>{this.props.recipe.name}</h5>
                     <h6>Ingredients:</h6>
                     <p>
-                        {this.props.recipe.ingredients.map(ingr => {
+                        {this.props.recipe.ingredients.map((ingr, i) => {
                             return (
-                                <li>
+                                <li key={i}>
                                     <span>{ingr.name} </span>
                                     <span>{ingr.quantity} </span>
                                     <span>{ingr.units} </span>
