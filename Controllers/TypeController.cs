2a5b781ab16a78c433475cc626c463a21fbdf5c5
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-27 09:57:45 -0400

    Code refactoring

--- Controllers/TypeController.cs
+++ Controllers/TypeController.cs
@@ -1,10 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using RecipeWebApplication.EntityDatabase.Models;
 using RecipeWebApplication.Repositories;
 using Type = RecipeWebApplication.EntityDatabase.Models.Type;
 
@@ -14,22 +9,11 @@ namespace RecipeWebApplication.Controllers
     [Route("[controller]")]
     public class TypeController : ControllerBase
     {
-
-
-        public TypeController()
-        {
-        }
         [HttpGet]
         public IEnumerable<Type> GetAllTypes(){
             var repository = new TypeRepository();
             var types = repository.GetAllTypes();
             return types;
-        }
-        [HttpPost]
-        public Type Insert (Type type){
-            var repository = new TypeRepository(); 
-            Type result = repository.Insert(type);
-            return result;
         }
     }
 }        
