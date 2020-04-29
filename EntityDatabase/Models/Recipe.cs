516905da469e7f98b7da639b4c5c503a2a641900
SvetlanaSurzhan <svetlana.d.surzhan@gmail.com>
2020-04-28 23:25:59 -0400

    Deleted type id and commit

--- EntityDatabase/Models/Recipe.cs
+++ EntityDatabase/Models/Recipe.cs
@@ -10,41 +10,5 @@ namespace RecipeWebApplication.EntityDatabase.Models
         public string Description { get; set; }
         public virtual ICollection<Ingredient> Ingredients { get; set; }
         public virtual Type Type { get; set; }
-        public int TypeId {get; set;}
-
-        // static void Test() 
-        // {
-        //     Recipe recipe = new Recipe()
-        //     {
-        //         RecipeId = 1,
-        //         Name = "Pancakes",
-        //         Picture = "",
-        //         Discription = "test",
-        //         Type = new Type()
-        //         {
-        //             TypeId = 11,
-        //             Name = "Breakfast",
-        //         },
-
-        //         Ingridients = new List<Ingridient>()
-        //         {
-        //             new Ingridient()
-        //             {
-        //                 IngridientId = 22,
-        //                 Name = "Flour",
-        //                 Quantity = 1.1,
-        //                 Units = "pound",
-        //                 RecipeId = 1,
-        //             },
-        //             new Ingridient()
-        //             {
-        //                 IngridientId = 23,
-        //                 Name = "Eg",
-        //                 Quantity = 2.0,
-        //                 Units = "ech",
-        //                 RecipeId = 1,
-        //             }
-        //         }
-        //     }
     }
 }
